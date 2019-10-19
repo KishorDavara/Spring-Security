8aea51c72f9a5e1135feb4a89d039926781a0b7a
Kishor <kishordavara.patel49@gmail.com>
2019-10-19 23:59:27 +0530

    chore(spring-security): basic authentication support

---
+++ src/main/java/com/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+/**
+ * @author kdavara
+ *
+ */
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// ADD users for in-memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		  .withUser(users.username("Kishor").password("user123").roles("EMPLOYEE"))
+		  .withUser(users.username("Niki").password("user123").roles("Manager"))
+		  .withUser(users.username("John").password("user123").roles("ADMIN"));
+	}
+  
+}
---
+++ src/main/java/com/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author kdavara
+ *
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
