41d5b23f9591393a20e1860336caae06dfe0f4df
Kishor Davara <kishordavara.patel49@gmail.com>
2023-04-19 16:30:46 +0530

    spring-security: initial commit

---
+++ src/main/java/com/express/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -0,0 +1,13 @@
+package com.express.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+	}
+
+}
