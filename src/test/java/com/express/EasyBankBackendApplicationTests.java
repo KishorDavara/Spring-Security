d107b08fe806e79629690e8206d2af4758a2ac0f
Kishor Davara <kishordavara.patel49@gmail.com>
2023-04-20 12:26:37 +0530

    setup easy bank backend app

--- src/test/java/com/express/EasyBankBackendApplicationTests.java
+++ src/test/java/com/express/EasyBankBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.express.springsecuritybasic;
+package com.express;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringsecuritybasicApplicationTests {
+class EasyBankBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
