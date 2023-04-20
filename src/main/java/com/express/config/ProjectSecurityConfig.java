b27348a1e4d3c2642c4082535b2b5f2225d3052b
Kishor Davara <kishordavara.patel49@gmail.com>
2023-04-20 15:39:25 +0530

    spring-security: different type of security configuration

--- src/main/java/com/express/config/ProjectSecurityConfig.java
+++ src/main/java/com/express/config/ProjectSecurityConfig.java
@@ -13,11 +13,40 @@ public class ProjectSecurityConfig {
     @Bean
     // ref: SpringBootWebSecurityConfiguration
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /*
+        * Custom security configuration
+        * */
         http.authorizeHttpRequests()
                 .requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
                 .requestMatchers("/contact","/notices").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
+
+        /*
+        * Configuration to deny all the requests coming towards the application
+        * */
+//        http.authorizeHttpRequests()
+//                .anyRequest().denyAll()
+//                .and().formLogin()
+//                .and().httpBasic();
+
+
+        /*
+         * Configuration to permit all the requests coming towards the application
+         * */
+//        http.authorizeHttpRequests()
+//                .anyRequest().permitAll()
+//                .and().formLogin()
+//                .and().httpBasic();
+
+        /**
+         * Configuration to authenticate every request : default behavior use by spring security
+         */
+//        http.authorizeHttpRequests()
+//                .anyRequest().authenticated()
+//                .and().formLogin()
+//                .and().httpBasic();
+
         return http.build();
     }
 }
