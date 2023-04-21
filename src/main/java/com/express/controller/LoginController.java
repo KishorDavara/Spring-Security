309d722c60abad457c9e176a4a9edd2c23149e45
Kishor Davara <kishordavara.patel49@gmail.com>
2023-04-21 16:26:21 +0530

    spring-security: new user registration

---
+++ src/main/java/com/express/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.express.controller;
+
+import com.express.model.Customer;
+import com.express.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @PostMapping("/register")
+    ResponseEntity<String> createUser(@RequestBody Customer customer) {
+        ResponseEntity response = null;
+        try {
+            Customer result = customerRepository.save(customer);
+            if (result != null) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("User registered successfully.");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + ex.getMessage());
+        }
+        return response;
+    }
+}
