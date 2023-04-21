7d1577e31c4f76732e6460b90235ae64573a082b
Kishor Davara <kishordavara.patel49@gmail.com>
2023-04-21 17:56:41 +0530

    spring-security: configure BCryptPasswordEncoder

--- src/main/java/com/express/config/ProjectSecurityConfig.java
+++ src/main/java/com/express/config/ProjectSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -98,12 +99,9 @@ public class ProjectSecurityConfig {
 //        return new JdbcUserDetailsManager(dataSource);
 //    }
 
-    /**
-     * NoOpPasswordEncoder is not recommended for production usage. Use only for non-prod
-     * @return PasswordEncoder
-     */
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/express/controller/LoginController.java
+++ src/main/java/com/express/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.express.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,15 @@ public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     ResponseEntity<String> createUser(@RequestBody Customer customer) {
         ResponseEntity response = null;
         try {
+            String hasedPassword = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hasedPassword);
             Customer result = customerRepository.save(customer);
             if (result != null) {
                 response = ResponseEntity
